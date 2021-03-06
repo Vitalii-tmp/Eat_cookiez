7106fb59572b205603e70001386e54a8129f7ea8
Vitalii-tmp <vitaliilchuk6@gmail.com>
2021-03-06 15:07:35 +0200

    move up

---
+++ Assets/Scripts/MoveUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUp : MonoBehaviour
+{
+    public float MoveSpeed;
+
+    void Update()
+    {
+        transform.position += transform.up * MoveSpeed * 10 * Time.deltaTime;
+    }
+}
