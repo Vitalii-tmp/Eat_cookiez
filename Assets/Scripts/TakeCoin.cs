9b21a76a1a197920aa82d40f7e853b57364d5884
yura <berun_ak20_@nuwm.edu.ua>
2021-03-09 16:59:59 +0200

    added coins

---
+++ Assets/Scripts/TakeCoin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeCoin : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
