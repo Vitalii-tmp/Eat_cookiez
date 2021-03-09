a4e53eb12cf7e586282d769794820c730350f266
Vitalii-tmp <vitaliilchuk6@gmail.com>
2021-03-09 13:10:31 +0200

    add move

--- Assets/Scripts/MoveUp.cs
+++ Assets/Scripts/MoveUp.cs
@@ -1,13 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class MoveUp : MonoBehaviour
+using UnityEngine.EventSystems;
+public class MoveUp : MonoBehaviour, IDragHandler, IBeginDragHandler
 {
     public float MoveSpeed;
+    private Vector3 dir;
+    public Transform pl_transform;
+    void Start()
+    {
+        dir = Vector3.up;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y)){
+            
+            if (eventData.delta.x > 0)
+            {
+                dir = Vector3.right;
+            }
+            if (eventData.delta.x < 0)
+            {
+                dir = Vector3.left;
+            }
+           
+        }
+        else
+        {
+            if (eventData.delta.y > 0)
+            {
+                dir = Vector3.up;
+            }
+        }
+    }
+
+   
+    public void OnDrag(PointerEventData eventData)
+    {
+       
+    }
 
     void Update()
     {
-        transform.position += transform.up * MoveSpeed * 10 * Time.deltaTime;
+        pl_transform.position += dir*MoveSpeed;
     }
 }
