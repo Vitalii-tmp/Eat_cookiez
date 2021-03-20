3d781c63a3af210bb1a87ffdf6e11aaad4c003dc
yura <berun_ak20_@nuwm.edu.ua>
2021-03-20 23:34:43 +0200

    added change animation and update vizual part

---
+++ Assets/Scripts/Game logic/ChangeAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    private void FixedUpdate()
+    {
+        if (PlayerPrefs.GetInt("skins") == 1) 
+        {
+            animator.SetBool("Cow", true);
+            animator.SetBool("Pig", false);
+            animator.SetBool("Sheep", false);
+        }
+        if (PlayerPrefs.GetInt("skins") == 2)
+        {
+            animator.SetBool("Cow", false);
+            animator.SetBool("Pig", true);
+            animator.SetBool("Sheep", false);
+        }
+        if (PlayerPrefs.GetInt("skins") == 3)
+        {
+            animator.SetBool("Cow", false);
+            animator.SetBool("Pig", false);
+            animator.SetBool("Sheep", true);
+        }
+    }
+
+   
+}
