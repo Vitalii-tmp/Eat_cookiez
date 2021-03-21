6982ab1b2f69af760a8eeb37e03fb4c7b09da658
yura <berun_ak20_@nuwm.edu.ua>
2021-03-22 00:27:19 +0200

    a

--- Assets/Scripts/Game logic/CameraMove.cs
+++ Assets/Scripts/Game logic/CameraMove.cs
@@ -8,8 +8,6 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
-    
-
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);   
     }
 }
---
+++ Assets/Scripts/Menu scripts/Music.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource audio;
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("music"))
+            PlayerPrefs.SetInt("music", 1);
+    }
+    public void MusicPause()
+    {
+        if (PlayerPrefs.GetInt("music") == 1)
+        {
+            audio.volume = 0;
+            PlayerPrefs.SetInt("music", 0);
+        }
+        else 
+        {
+            audio.volume = 0.3F;
+            PlayerPrefs.SetInt("music", 1);
+        }
+    }
+}
