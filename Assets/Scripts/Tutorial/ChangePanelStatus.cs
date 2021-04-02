dbaf634e75e00c612a51ebd8520026ba8326026c
Vitalii-tmp <vitaliilchuk6@gmail.com>
2021-04-02 22:20:18 +0300

    Tutorial added

---
+++ Assets/Scripts/Tutorial/ChangePanelStatus.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangePanelStatus : MonoBehaviour
+{
+
+    public GameObject ImageSwipeRight;
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ImageSwipeRight.SetActive(true);
+            Time.timeScale = 0;
+        }
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            ImageSwipeRight.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+
+
+
+
+}
